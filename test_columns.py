461565d5842fcea5f41c4a5ccb233d88635d391a
Narahari Sai Jaswanth <sainarahari446@gmail.com>
2026-06-03 13:45:16 +0530

    Remove

--- test_columns.py
+++
@@ -1,7 +0,0 @@
-# test_columns.py
-
-from src.data_ingestion import load_data
-
-df = load_data()
-
-print(df.columns.tolist())
--- test_sales.py
+++
@@ -1,9 +0,0 @@
-# test_sales.py
-
-from database.mongodb_connection import db
-
-print(db.list_collection_names())
-
-print(
-    db["sales_data"].count_documents({})
-)
